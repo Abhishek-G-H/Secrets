f8d1f48915f7b7f76ed3a894b841704299918eb6
Abhishek-G-H <ghabhishek2003@gmail.com>
2023-07-14 14:00:33 +0530

    Lvl-3 Hashing Passwords using md5

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require ("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,7 +21,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User",userSchema);
 
@@ -39,7 +40,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save().then(function(){
         res.render("secrets");
@@ -51,7 +52,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const userEmail = req.body.username;
-    const userPassword = req.body.password;
+    const userPassword = md5(req.body.password);
     User.findOne({email: userEmail}).then(function(foundUser){
         if(foundUser){
             if(foundUser.password===userPassword){
